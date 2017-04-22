7618a6706bfdfb621af1e37a2b69baae39a77728
Diego Ximenes <dxmendes1@gmail.com>
2017-04-22 19:48:55 -0300

    codejam template

--- judges/codejam/template.cpp
+++ judges/codejam/template.cpp
@@ -9,8 +9,16 @@ typedef long long ll;
 #define INF 0x3f3f3f3f
 #define INFLL 0x3f3f3f3f3f3f3f3fLL
 const double PI = acos(-1);
+const double EPS = 1e-9;
+
+inline int cmp_double(double x, double y, double tol = EPS) {
+    // (x < y): -1, (x == y): 0, (x > y): 1
+    return (x <= y + tol) ? (x + tol < y) ? -1 : 0 : 1;
+}
 
 int main() {
+    ios::sync_with_stdio(false);
+
     int t;
     scanf("%d", &t);
     for (int tc = 1; tc <= t; ++tc) {
