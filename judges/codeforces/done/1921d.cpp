71851efa48f267b0512bfeba5c9660581f44ad05
Diego Ximenes <dxmendes1@gmail.com>
2025-02-08 17:34:29 -0300

    codeforces

---
+++ judges/codeforces/done/1921d.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+#ifdef LOCAL
+#define debug(...)                    \
+    {                                 \
+        fprintf(stderr, __VA_ARGS__); \
+        fflush(stderr);               \
+    }
+#else
+#define debug(...)
+#endif
+
+const int INF = 0x3f3f3f3f;
+const ll INFLL = 0x3f3f3f3f3f3f3f3fLL;
+const double PI = acos(-1);
+const double EPS = 1e-9;
+
+inline int cmp_double(double x, double y, double tol = EPS) {
+    // (x < y): -1, (x == y): 0, (x > y): 1
+    return (x <= y + tol) ? (x + tol < y) ? -1 : 0 : 1;
+}
+
+template <class T>
+inline void print_array(T *v, int n) {
+    if (n > 0) {
+        cout << v[0];
+    }
+    for (int i = 1; i < n; ++i) {
+        cout << " " << v[i];
+    }
+    cout << endl;
+}
+
+template <class T>
+inline void read_array(T *v, int n, int start = 0) {
+    for (int i = start; i < start + n; ++i) {
+        cin >> v[i];
+    }
+}
+
+template <class T>
+inline void print_vector(const vector<T> &v) {
+    if (!v.empty()) {
+        cout << v[0];
+    }
+    for (int i = 1; i < (int) v.size(); ++i) {
+        cout << " " << v[i];
+    }
+    cout << endl;
+}
+
+template <class T>
+inline void read_vector(vector<T> &v, int n, int start = 0) {
+    for (int i = start; i < start + n; ++i) {
+        cin >> v[i];
+    }
+}
+
+#define MAX 2000000
+
+ll a[MAX], b[MAX];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+
+        cin >> n >> m;
+        read_array(a, n);
+        read_array(b, m);
+
+        sort(a, a + n);
+        sort(b, b + m);
+
+        int lb = 0, rb = m - 1;
+        int la = 0, ra = n -1;
+        ll ans = 0;
+        while (la <= ra) {
+            if (b[rb] - a[la] > a[ra] - b[lb]) {
+                ans += b[rb] - a[la];
+                la++;
+                rb--;
+            } else {
+                ans += a[ra] - b[lb];
+                lb++;
+                ra--;
+            }
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
